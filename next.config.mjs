f3b8789b0722a20c094f9bb3c98e167b7fd6559b
jair <jmisoardi@hotmail.com>
2026-09-21 09:23:38 -0300

    Corregir rastreo de SWC helpers para Hostinger

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  outputFileTracingIncludes: {
+    '/*': ['./node_modules/@swc/helpers/esm/**/*'],
+  },
 }
 
 export default nextConfig
